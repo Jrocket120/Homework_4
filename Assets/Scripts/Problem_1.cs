23c116b6822c578a964f55b6c8a4758ca19b9d92
Jrocket120 <101006636+Jrocket120@users.noreply.github.com>
2022-07-22 16:12:27 -0400

    progress

--- Assets/Scripts/Problem_1.cs
+++ Assets/Scripts/Problem_1.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class Problem_1 : MonoBehaviour
 {
     //Call your custom functions and print out the returns values of boxes 12,4 and 13.5f by 1.3f.
-    //You should have 48 and 17.55 as your return print outs. 
+    //You should have 32 and 29.6f as your return print outs. 
     void Start()
     {
-        
+        int a = perimeter(12, 4);
+        float b = perimeter(13.5f, 1.3f);
+        print(a);
+        print(b);
     }
+    int perimeter(int length,int width)
+    
+    {
+        return 2 * length + 2 * width;
+    }
+    float perimeter(float length, float width)
+    {
+        return 2 * length + 2 * width;
+    }
+    
+
+}
+        
+    
 
     //Part 1: 
     //Create a function called Perimeter that takes in two parameters of int length and int width
@@ -18,4 +35,4 @@ public class Problem_1 : MonoBehaviour
 
     //Part 2: 
     //Overload the function by creating a float version of the same function 
-}
+
--- Assets/Scripts/Problem_2.cs
+++ Assets/Scripts/Problem_2.cs
@@ -7,11 +7,28 @@ public class Problem_2 : MonoBehaviour
     //Call your function using an int[] array = new int[10]{1,2,3,4,5,6,7,8,9,10};
     //Your return values should be a List<int> that holds {1,3,5,7,9}
     void Start()
-    {
-        
+    { int[] array = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+
     }
+    List<int> function(int[] line) {
+        List<int> empty = new List<int>();
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                empty.Add(i);
+            }
+
+
+        } return (empty);
+    }
+}
+    
 
     //Create a function called OnlyOddOnes that takes in an int array as a parameter and returns 
     //a List of integers that were odd inside the array
 
-}
+
